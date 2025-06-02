e1ad85f6c5941e723aca5bac50fea710b28de644
lautamendoza <lautamendoza122@gmail.com>
2025-06-02 16:32:51 -0300

    cambios 2 de junio

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -103,10 +103,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+import os
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'p1ecommerce', 'static'),
+]
+
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django import views
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,3 +15,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'p1ecommerce', 'static'))
