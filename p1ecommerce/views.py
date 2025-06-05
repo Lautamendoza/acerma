e5ab62ace2594226c887af84b00ba63a57e536b0
lautamendoza <lautamendoza122@gmail.com>
2025-06-05 10:23:18 -0300

    ANDA EL BUSCADOR

--- p1ecommerce/views.py
+++ p1ecommerce/views.py
@@ -3,28 +3,30 @@ from .models import Product, Category
 from cart.forms import CartAddProductForm
 from django.db.models import Q
 
-def product_list(request, category_slug=None):
-    queryset = request.GET.get('buscar')
-    if queryset:
-        products = Product.objects.filter(
-            Q(name__icontains=queryset),
-            Q(description__icontains=queryset)
-        ).distinct()
 
+def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
 
     if category_slug:
-        category = get_object_or_404(Category,
-                                     slug= category_slug )
-        products= products.filter(category=category)
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
 
-    return render(request,
-                      'tienda/products/list.html',
-                      {'category': category,
-                       'categories': categories,
-                       'products': products})
+    query = request.GET.get('search')  # Obtener texto del input
+    if query:
+        products = products.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
+
+    return render(request, 'tienda/list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products
+    })
+
+    
+    
 
 
 def product_detail(request, id, slug):
