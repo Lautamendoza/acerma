aa71e9c44ff4845cd4acdf2ca97f63dd0522cc45
lautamendoza <lautamendoza122@gmail.com>
2025-06-12 13:19:25 -0300

    LISTO

--- p1ecommerce/views.py
+++ p1ecommerce/views.py
@@ -3,7 +3,8 @@ from .models import Product, Category
 from cart.forms import CartAddProductForm
 from django.db.models import Q
 from django.contrib.auth import authenticate, login as auth_login
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User     
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 def product_list(request, category_slug=None):
